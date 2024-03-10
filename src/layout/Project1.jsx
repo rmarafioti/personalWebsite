f497f3aad2bb8ff92495284330d628ef2580cfb6
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-10 16:07:46 -0500

    placeholder text and images for Project pages

--- src/layout/Project1.jsx
+++ src/layout/Project1.jsx
@@ -4,7 +4,33 @@ export default function Project1() {
   return (
     <>
       <h1 id="work">PROJECT1</h1>
-      <Link to="/work">BACK</Link>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>IMAGES OF UI SKETCHES</p>
+        <button>CLICK THROUGH IMAGES</button>
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>IMAGE OF PROJECT WIREFRAME</p>
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+          aenean pharetra magna ac. Neque convallis a cras semper. Scelerisque
+          fermentum dui faucibus in ornare. Amet porttitor eget dolor morbi.
+          Nunc sed augue lacus viverra vitae congue. Sit amet massa vitae tortor
+          condimentum. Tristique senectus et netus et malesuada fames ac turpis.
+          Tellus mauris a diam maecenas sed. Tellus rutrum tellus pellentesque
+          eu tincidunt. Scelerisque purus semper eget duis at tellus at.
+          Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Non odio
+          euismod lacinia at quis risus sed vulputate. Fermentum posuere urna
+          nec tincidunt praesent semper feugiat nibh.
+        </p>
+        <button>LINK TO PROJECT GITHUB</button>
+      </section>
+      <Link to="/work">BACK TO PORTFOLIO</Link>
     </>
   );
 }
