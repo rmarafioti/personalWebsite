91a9e8d239d0149cff6316a3c0f3115b9b61e0a3
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-03-09 21:33:09 -0600

    added placeholder images & text

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -2,6 +2,25 @@ export default function Home() {
   return (
     <>
       <h1 id="home">HOME</h1>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+      </section>
+      <article>
+        <h1>LARGE TEXT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+          aenean pharetra magna ac. Neque convallis a cras semper. Scelerisque
+          fermentum dui faucibus in ornare. Amet porttitor eget dolor morbi.
+          Nunc sed augue lacus viverra vitae congue. Sit amet massa vitae tortor
+          condimentum. Tristique senectus et netus et malesuada fames ac turpis.
+          Tellus mauris a diam maecenas sed. Tellus rutrum tellus pellentesque
+          eu tincidunt. Scelerisque purus semper eget duis at tellus at.
+          Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Non odio
+          euismod lacinia at quis risus sed vulputate. Fermentum posuere urna
+          nec tincidunt praesent semper feugiat nibh.
+        </p>
+      </article>
     </>
   );
 }
--- src/layout/Work.jsx
+++ src/layout/Work.jsx
@@ -4,9 +4,40 @@ export default function Work() {
   return (
     <>
       <h1 id="work">WORK</h1>
-      <Link to="/work/project1">Project 1</Link>
-      <Link to="/work/project2">Project 2</Link>
-      <Link to="/work/project3">Project 3</Link>
+      <section>
+        <h1>LARGE TEXT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+          aenean pharetra magna ac. Neque convallis a cras semper. Scelerisque
+          fermentum dui faucibus in ornare. Amet porttitor eget dolor morbi.
+          Nunc sed augue lacus viverra vitae congue. Sit amet massa vitae tortor
+          condimentum. Tristique senectus et netus et malesuada fames ac turpis.
+          Tellus mauris a diam maecenas sed. Tellus rutrum tellus pellentesque
+          eu tincidunt. Scelerisque purus semper eget duis at tellus at.
+          Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Non odio
+          euismod lacinia at quis risus sed vulputate. Fermentum posuere urna
+          nec tincidunt praesent semper feugiat nibh.
+        </p>
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>Some text about the project</p>
+        <Link to="/work/project1">Project 1</Link>
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>Some text about the project</p>
+        <Link to="/work/project2">Project 2</Link>
+      </section>
+      <section>
+        <img src="https://picsum.photos/seed/picsum/200/300" />
+        <p>Some text about the project</p>
+        <Link to="/work/project3">Project 3</Link>
+      </section>
     </>
   );
 }
