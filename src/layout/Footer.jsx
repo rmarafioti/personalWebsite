f93627367d5983db8b6c6b682edb8b65b58ca669
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-04-15 13:30:34 -0500

    fixed footer elements and aligned

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,20 +6,13 @@
 
 export default function Footer() {
   return (
-    <ul id="footer">
-      <li>
-        <h1>FOOTER</h1>
-      </li>
-      <li>
-        <a target="_blank" href="https://www.linkedin.com/in/richmarafioti/">
-          <img src="https://res.cloudinary.com/dzpne110u/image/upload/v1713204026/personalWebsite/linkedIn_kfa4tf.png" />
-        </a>
-      </li>
-      <li>
-        <a href="mailto:richmarafioti@gmail.com">
-          <span class="material-symbols-outlined">mail</span>
-        </a>
-      </li>
-    </ul>
+    <section id="footer">
+      <a target="_blank" href="https://www.linkedin.com/in/richmarafioti/">
+        <img src="https://res.cloudinary.com/dzpne110u/image/upload/v1713204026/personalWebsite/linkedIn_kfa4tf.png" />
+      </a>
+      <a href="mailto:richmarafioti@gmail.com">
+        <span class="material-symbols-outlined">mail</span>
+      </a>
+    </section>
   );
 }
