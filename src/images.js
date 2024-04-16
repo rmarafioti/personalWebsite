f1d7d26da8e5b7a60af2acdc786ff13c57d34344
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-04-16 08:15:09 -0500

    created wireframe images for book buddy and imported to functional gallery

--- src/images.js
+++ src/images.js
@@ -1,14 +1,14 @@
 export const galleryPics = [
   {
     id: 1,
-    image: "../images/squiggle1.jpg",
+    image: "../images/bBwF2.png",
   },
   {
     id: 2,
-    image: "../images/squiggle2.jpg",
+    image: "../images/bBwF3.png",
   },
   {
     id: 3,
-    image: "../images/squiggle3.jpg",
+    image: "../images/bBwF1.png",
   },
 ];
