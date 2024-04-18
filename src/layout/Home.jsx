dd3a377aca7c562e772c9d7e67ec5a307c5cddb9
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-04-18 08:36:37 -0500

    added landing page image and formatting

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -9,28 +9,29 @@ import React, { useState } from "react";
 export default function Home() {
   return (
     <>
-      <h1 id="home">HOME</h1>
-      <section>
-        <img
-          id="homePhoto"
-          src="https://picsum.photos/seed/picsum/200/300"
-          alt="main image for homepage"
-        />
-      </section>
-      <article>
-        <h1>WELCOME!</h1>
-        <p>
-          I’m Rich Marafioti, a Fullstack Developer and Designer living in
-          Chicago. With 20 years of independence in art and design, and a decade
-          of owning and operating my own small business, I blend creativity with
-          functionality.
-        </p>
-        <h2>MY MISSION?</h2>
-        <p>
-          To seamlessly integrate design and functionality, alongside aesthetics
-          and effective communication, ensuring your digital presence is as
-          vibrant and dynamic as your vision.
-        </p>
+      <article id="home">
+        <section id="homePhotoContainer">
+          <img
+            id="homePhoto"
+            src="https://res.cloudinary.com/dzpne110u/image/upload/v1713446166/personalWebsite/Untitled_Artwork_3_xm1esg.png"
+            alt="main image for homepage"
+          />
+        </section>
+        <section id="pitchSection">
+          <h2>WELCOME!</h2>
+          <p>
+            I’m Rich Marafioti, a Fullstack Developer and Designer living in
+            Chicago. With 20 years of independence in art and design, and a
+            decade of owning and operating my own small business, I blend
+            creativity with functionality.
+          </p>
+          <h2>MY MISSION?</h2>
+          <p>
+            To seamlessly integrate design and functionality, alongside
+            aesthetics and effective communication, ensuring your digital
+            presence is as vibrant and dynamic as your vision.
+          </p>
+        </section>
       </article>
     </>
   );
