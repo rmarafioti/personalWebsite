3241141ae83f8d79e1f2354a4b4634e361a366d3
Richard Marafioti <marafioti@MacBook-Air.local>
2024-05-09 12:54:13 -0500

    finished styling revisons on about page

--- src/layout/About.jsx
+++ src/layout/About.jsx
@@ -13,61 +13,16 @@ export default function About() {
             src="https://res.cloudinary.com/dzpne110u/image/upload/v1713531473/personalWebsite/webShot_in9bq6.png"
             alt="personal headshot for about me page"
           />
-          <h4 id="toolkitTitle">TECH TOOLKIT:</h4>
-          <p className="aboutContent">
-            Git | Node | HTML | CSS | JavaScript | React | Redux | Express |
-            Prisma | Postman | Photoshop | Procreate | Figma
-          </p>
-        </div>
-        <article>
-          <div id="workTimeline">
-            <section id="workTimelineHeader">
-              <p id="workTimeLineTag">From Creativity to Code</p>
-              <img
-                src="https://res.cloudinary.com/dzpne110u/image/upload/v1713534663/personalWebsite/Untitled_Artwork_sxtwpl.png"
-                alt="illustration of a rubber duck as an artist with paintbrush and palette"
-              />
-            </section>
-            <h4>RISD Illustration Graduate & Visual Artist: </h4>
-            <h6 className="jobTag">
-              Bachelor's Degree, Illustration Department: 1999 - 2003
-            </h6>
-            <p className="aboutContent">
-              My professional journey began in the world of visual arts, where I
-              further developed my design skills and aesthetic eye.
-            </p>
-            <h4>Tattoo Artist:</h4>
-            <h6 className="jobTag">
-              Freelance Tattooer / Graphic Artist: 2005 - 2015
-            </h6>
-            <p className="aboutContent">
-              For 20 years, I've brought stories to life through my work in 2D
-              and 3D design, perfecting my technical craft in the art form of
-              tattooing and custom personalized designing.
-            </p>
-            <h4>Business Owner:</h4>
-            <h6 className="jobTag">
-              Wild Wind Tattoo, Owner / Operator: 2015 - 2024
-            </h6>
-            <p className="aboutContent">
-              Managing my own small business for 10 years taught me the ropes of
-              client relations, project management, and strategic planning.
-            </p>
-            <h4>Fullstack Developer:</h4>
-            <h6 className="jobTag">
-              Fullstack Academy: 1/24 - 4/24 focused on front / backend
-              development, & databases.
-            </h6>
-            <p className="aboutContent">
-              Today, I blend these experiences to build creative digital
-              solutions. Whether leading a team, collaborating, or flying solo,
-              I thrive on turning thoughtful concepts into functional realities.
+          <div>
+            <h4 id="toolkitTitle">Tech Toolkit</h4>
+            <p id="skills">
+              Git | Node | HTML | CSS | JavaScript | React | Redux | Express |
+              Prisma | Postman | Photoshop | Procreate | Figma
             </p>
           </div>
-
           <section id="funFactsSection">
             <div id="facts">
-              <h3 id="funFactsTitle">FUN FACTS:</h3>
+              <h3 id="funFactsTitle">Fun Facts</h3>
               <ul id="list">
                 <li className="listItem">
                   <span className="material-symbols-outlined">family_star</span>
@@ -85,6 +40,48 @@ export default function About() {
               </ul>
             </div>
           </section>
+        </div>
+        <article>
+          <section id="workTimelineHeader">
+            <p id="workTimeLineTag">From creativity to code</p>
+          </section>
+          <header className="work">
+            RISD Illustration Graduate & Visual Artist:{" "}
+          </header>
+          <h6 className="jobTag">
+            Bachelor's Degree, Illustration Department 1999 - 2003
+          </h6>
+          <p className="aboutContent">
+            My professional journey began in the world of visual arts, where I
+            further developed my design skills and aesthetic eye.
+          </p>
+          <header className="work">Tattoo Artist:</header>
+          <h6 className="jobTag">
+            Freelance Tattooer / Graphic Artist 2005 - 2015
+          </h6>
+          <p className="aboutContent">
+            For 20 years, I've brought stories to life through my work in 2D and
+            3D design, perfecting my technical craft in the art form of
+            tattooing and custom personalized designing.
+          </p>
+          <header className="work">Business Owner:</header>
+          <h6 className="jobTag">
+            Wild Wind Tattoo, Owner / Operator 2015 - 2024
+          </h6>
+          <p className="aboutContent">
+            Managing my own small business for 10 years taught me the ropes of
+            client relations, project management, and strategic planning.
+          </p>
+          <header className="work">Fullstack Developer:</header>
+          <h6 className="jobTag">
+            Fullstack Academy 1/24 - 4/24 focused on front / backend
+            development, & databases.
+          </h6>
+          <p className="aboutContent">
+            Today, I blend these experiences to build creative digital
+            solutions. Whether leading a team, collaborating, or flying solo, I
+            thrive on turning thoughtful concepts into functional realities.
+          </p>
         </article>
       </section>
     </>
