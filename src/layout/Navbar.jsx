0892d93ce9086748948c7926c45a474b60837531
Richard Marafioti <marafioti@MacBook-Air.local>
2024-05-09 10:58:37 -0500

    edited navbar styling / layout

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -10,25 +10,16 @@ export default function Navbar() {
   return (
     <nav>
       <div id="navHeader">
-        <img
-          id="headerFirstName"
-          src="https://res.cloudinary.com/dzpne110u/image/upload/v1713533982/personalWebsite/Untitled_Artwork_4_egrdlw.png"
-          alt="image of landing page for project"
-        />
-        <img
-          id="headerLastName"
-          src="https://res.cloudinary.com/dzpne110u/image/upload/v1713533993/personalWebsite/Untitled_Artwork_5_oz5h3m.png"
-          alt="image of landing page for project"
-        />
-        <p>fullstack developer / designer</p>
+        <NavLink to="/">
+          <img
+            id="navImg"
+            src="https://res.cloudinary.com/dzpne110u/image/upload/v1713534663/personalWebsite/Untitled_Artwork_sxtwpl.png"
+            alt="illustration of a rubber duck as an artist with paintbrush and palette"
+          />
+        </NavLink>
       </div>
       <div id="navLinks">
         <button className="navButton">
-          <NavLink className="navLink" to="/">
-            HOME
-          </NavLink>
-        </button>
-        <button className="navButtonMiddle">
           <NavLink className="navLink" to="/work">
             WORK
           </NavLink>
