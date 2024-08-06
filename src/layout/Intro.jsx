172a30cc4bd264239b801a052b39de4de403029e
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-08-05 20:17:14 -0500

    created single page layout

---
+++ src/layout/Intro.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <>
+      <article id="home">
+        <section id="homePhotoContainer">
+          <img
+            id="homePhoto"
+            src="https://res.cloudinary.com/dzpne110u/image/upload/v1713550153/personalWebsite/Untitled_Artwork_9_aszsfu.png"
+            alt="main image for homepage"
+          />
+        </section>
+        <section id="pitchSection">
+          <div className="pitchSectionParagraphOne">
+            <header id="welcomeLine">Welcome!</header>
+            <p className="homeContentOne">
+              I’m <em>Rich Marafioti</em>, a Fullstack Developer and Designer
+              living in Chicago. With 20 years of independence in art and
+              design, and a decade of owning and operating my own small
+              business, I blend creativity with functionality.
+            </p>
+          </div>
+          <div className="pitchSectionParagraph">
+            <p className="homeContentTwo">
+              <em>My mission?</em> To seamlessly integrate design and
+              functionality, alongside aesthetics and effective communication,
+              ensuring your digital presence is as vibrant and dynamic as your
+              vision.
+            </p>
+          </div>
+        </section>
+      </article>
+    </>
+  );
+}
