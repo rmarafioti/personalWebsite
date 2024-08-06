ae8aca641e50a462ecbc09c50875d16712db6000
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-08-06 09:23:07 -0500

    adjusted layout design on about section and edited / revised about css

--- src/features/About.jsx
+++ src/features/About.jsx
@@ -19,88 +19,79 @@ export default function About() {
   };
 
   return (
-    <article id="about">
-      <section id="aboutIntro">
-        <article>
-          <div id="aboutImageContainer">
-            <img
-              src="https://res.cloudinary.com/dzpne110u/image/upload/v1713531473/personalWebsite/webShot_in9bq6.png"
-              alt="personal headshot for about me page"
-            />
-          </div>
-          <div id="skillSection">
-            <h4 id="skillsTitle">SKILLS</h4>
-            <p id="skills">
-              Git | Node | HTML | CSS | JavaScript | React | Redux | Express |
-              Prisma | Postman | Photoshop | Procreate | Figma
-            </p>
-
-            <button id="resumeButton" onClick={openPdfInNewTab}>
-              View Resume
-            </button>
-          </div>
-        </article>
-        <article id="experienceSection">
-          <section id="workTimelineHeader">
-            <p id="workTimeLineTag">EXPERIENCE</p>
-          </section>
-          <header className="work">
-            RISD Illustration Graduate & Visual Artist:{" "}
-          </header>
-          <h6 className="jobTag">
-            Bachelor's Degree, Illustration Department 1999 - 2003
-          </h6>
-          <p className="aboutContent">
-            My professional journey began in the world of visual arts, where I
-            further developed my design skills and aesthetic eye.
-          </p>
-          <header className="work">Tattoo Artist:</header>
-          <h6 className="jobTag">
-            Freelance Tattooer / Graphic Artist 2005 - 2015
-          </h6>
-          <p className="aboutContent">
-            For 20 years, I've brought stories to life through my work in 2D and
-            3D design, perfecting my technical craft in the art form of
-            tattooing and custom personalized designing.
-          </p>
-          <header className="work">Business Owner:</header>
-          <h6 className="jobTag">
-            Wild Wind Tattoo, Owner / Operator 2015 - 2024
-          </h6>
-          <p className="aboutContent">
-            Managing my own small business for 10 years taught me the ropes of
-            client relations, project management, and strategic planning.
-          </p>
-          <header className="work">Fullstack Developer:</header>
-          <h6 className="jobTag">
-            Fullstack Academy 1/24 - 4/24 focused on front / backend
-            development, & databases.
-          </h6>
-          <p className="aboutContent">
-            Today, I blend these experiences to build creative digital
-            solutions. Whether leading a team, collaborating, or flying solo, I
-            thrive on turning thoughtful concepts into functional realities.
-          </p>
-          <div id="factsSection">
-            <h3 id="funFactsTitle">Fun Facts</h3>
-            <ul id="list">
-              <li className="listItem">
-                <IoCheckmark />
-                Toured Europe in a band at the age of 16.
-              </li>
-              <li className="listItem">
-                <IoCheckmark />
-                Clinched 2nd place in a demolition derby with an old car of
-                mine.
-              </li>
-              <li className="listItem">
-                <IoCheckmark />
-                Crossed the finish line at the Chicago Marathon four times.
-              </li>
-            </ul>
-          </div>
-        </article>
+    <article className="about">
+      <img
+        className="headshot"
+        src="https://res.cloudinary.com/dzpne110u/image/upload/v1713531473/personalWebsite/webShot_in9bq6.png"
+        alt="personal headshot for about me page"
+      />
+      <div className="aboutSection">
+        <h2 className="title">SKILLS</h2>
+        <p className="skills">
+          Git | Node | HTML | CSS | JavaScript | React | Redux | Next.js |
+          Express | Prisma | Postman | Photoshop | Procreate | Figma
+        </p>
+        <button className="resumeButton" onClick={openPdfInNewTab}>
+          View Resume
+        </button>
+      </div>
+      <section className="aboutSection">
+        <p className="title">EXPERIENCE</p>
+        <header className="work" id="topWorkTitle">
+          RISD Illustration Graduate & Visual Artist:{" "}
+        </header>
+        <h6 className="jobTag">
+          Bachelor's Degree, Illustration Department 1999 - 2003
+        </h6>
+        <p className="aboutContent">
+          My professional journey began in the world of visual arts, where I
+          further developed my design skills and aesthetic eye.
+        </p>
+        <header className="work">Tattoo Artist:</header>
+        <h6 className="jobTag">
+          Freelance Tattooer / Graphic Artist 2005 - 2015
+        </h6>
+        <p className="aboutContent">
+          For 20 years, I've brought stories to life through my work in 2D and
+          3D design, perfecting my technical craft in the art form of tattooing
+          and custom personalized designing.
+        </p>
+        <header className="work">Business Owner:</header>
+        <h6 className="jobTag">
+          Wild Wind Tattoo, Owner / Operator 2015 - 2024
+        </h6>
+        <p className="aboutContent">
+          Managing my own small business for 10 years taught me the ropes of
+          client relations, project management, and strategic planning.
+        </p>
+        <header className="work">Fullstack Developer:</header>
+        <h6 className="jobTag">
+          Fullstack Academy 1/24 - 4/24 focused on front / backend development,
+          & databases.
+        </h6>
+        <p className="aboutContent">
+          Today, I blend these experiences to build creative digital solutions.
+          Whether leading a team, collaborating, or flying solo, I thrive on
+          turning thoughtful concepts into functional realities.
+        </p>
       </section>
+      <div className="aboutSection">
+        <h3 className="funFactsTitle">Fun Facts</h3>
+        <ul className="list">
+          <li className="listItem">
+            <IoCheckmark className="checkmark" />
+            Toured Europe for a whole summer in a band at the age of 16.
+          </li>
+          <li className="listItem">
+            <IoCheckmark className="checkmark" />
+            Clinched 2nd place in a demolition derby with an old car of mine.
+          </li>
+          <li className="listItem">
+            <IoCheckmark className="checkmark" />
+            Crossed the finish line at the Chicago Marathon four times.
+          </li>
+        </ul>
+      </div>
     </article>
   );
 }
