306e4660493cc08caef974c503f70c1973a5e2d9
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-08-07 15:45:57 -0500

    implemented scroll to top for work carousel

--- src/features/Work.jsx
+++ src/features/Work.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { scroller } from "react-scroll";
 import { motion } from "framer-motion";
 import useScrollAnimation from "../hooks/useScrollAnimation";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
@@ -12,15 +13,26 @@ export default function Work() {
   const { ref, controls, sectionVariants } = useScrollAnimation();
   const [activeSlide, setActiveSlide] = useState(0);
 
-  const handleNext = () => {
-    setActiveSlide((prev) => (prev + 1) % CarouselContent.length);
+  // Function to scroll to the top of the "Work" section
+  const scrollToTop = () => {
+    scroller.scrollTo("work", {
+      duration: 500,
+      delay: 0,
+      smooth: "easeInOutQuart",
+      offset: -10, // Adjust offset if needed for header
+    });
   };
 
-  const handlePrev = () => {
+  const handleNext = () => {
+    setActiveSlide((prev) => (prev + 1) % CarouselContent.length);
+    scrollToTop();
+  };
+
+  /*const handlePrev = () => {
     setActiveSlide(
       (prev) => (prev - 1 + CarouselContent.length) % CarouselContent.length
     );
-  };
+  };*/
 
   return (
     <motion.section
@@ -42,7 +54,10 @@ export default function Work() {
               <FaCircle
                 key={index}
                 className={`indicator ${index === activeSlide ? "active" : ""}`}
-                onClick={() => setActiveSlide(index)}
+                onClick={() => {
+                  setActiveSlide(index);
+                  scrollToTop();
+                }}
                 style={{
                   color:
                     index === activeSlide ? "rgb(162, 106, 105)" : "lightgrey",
